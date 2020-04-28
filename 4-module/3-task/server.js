d1db3c2c778b04f786ca411460ad8d0406bdb35f
m.smerdov <m.smerdov@reactive-agency.ru>
2020-04-28 17:21:19 +0300

    4 блок 3 задача готова

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -9,9 +10,20 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+    return;
+  } else if (!fs.existsSync(filepath)) {
+    res.statusCode = 404;
+    res.end();
+    return;
+  }
   switch (req.method) {
     case 'DELETE':
-
+      fs.unlink(filepath, () => {});
+      res.statusCode = 200;
+      res.end();
       break;
 
     default:
