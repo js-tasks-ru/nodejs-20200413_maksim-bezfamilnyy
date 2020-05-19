94b5dc379d43be2b8d59a92567b5cac5932c8fff
m.smerdov <m.smerdov@reactive-agency.ru>
2020-05-19 16:42:00 +0300

    7 module 1 task done

--- 7-module/1-task/libs/strategies/local.js
+++ 7-module/1-task/libs/strategies/local.js
@@ -1,8 +1,18 @@
+const User = require('../../models/User');
 const LocalStrategy = require('passport-local').Strategy;
 
 module.exports = new LocalStrategy(
     {usernameField: 'email', session: false},
     function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
+      User.findOne({ email: email }, async function (err, user) {
+        if (!user) {
+          return done(null, false, 'Нет такого пользователя');
+        }
+        const isPasswordValid = await user.checkPassword(password);
+        if (!isPasswordValid) {
+          return done(null, false, 'Неверный пароль');
+        }
+        return done(null, user);
+      });
     }
 );
