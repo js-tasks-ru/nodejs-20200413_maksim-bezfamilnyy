7108ea8fcbc09b8f32308018fecbf51b5eb0831d
m.smerdov <m.smerdov@reactive-agency.ru>
2020-05-26 13:27:06 +0300

    9mod 1task done

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,5 +1,16 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  const messages = await Message.find({}).limit(10);
+  const out = [];
+  await messages.map((message) => {
+    out.push({
+      date: message.date,
+      text: message.text,
+      id: message._id,
+      user: message.user,
+    });
+  });
+  ctx.body = {messages: [out[0]]};
+  return next();
 };
